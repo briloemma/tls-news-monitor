4c5986a139b06ac80bd5184be4104239f0922aa7
briloemma <115700291+briloemma@users.noreply.github.com>
2026-03-12 17:06:39 +0300

    Update check.py

--- check.py
+++ check.py
@@ -24,6 +24,7 @@ def get_latest_news():
     valid_news = [
         n for n in data.get("data", [])
         if n.get("status") == "published"
+        and "byMSQ2it" in n.get("tags", [])
         #and n.get("show_on_homepage") is True
         #and n.get("tenant") == "visas-it"
         #and n.get("publish_date") <= today.isoformat()
